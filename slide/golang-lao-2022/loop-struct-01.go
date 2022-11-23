f055a68f043008e3ad279135a6564dca806e368f
Thanatat Tamtan <acoshift@gmail.com>
2022-11-23 18:23:39 +0700

    update

---
+++ slide/golang-lao-2022/loop-struct-01.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// START OMIT
+type X struct {
+	ID int
+}
+
+func main() {
+	xs := []X{
+		{1},
+		{2},
+		{3},
+	}
+	var rs []*X
+	for _, x := range xs {
+		rs = append(rs, &x)
+	}
+
+	for _, r := range rs {
+		fmt.Println(r.ID)
+	}
+}
+
+// END OMIT
